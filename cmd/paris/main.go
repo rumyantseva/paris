0b2b5f011b5c3fce46a484a4a030a3eea67debda
Elena Grahovac <elena@grahovac.me>
2019-09-28 10:16:33 +0200

    Addeed a routrer

--- cmd/paris/main.go
+++ cmd/paris/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	r := mux.NewRouter()
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
+		Handler: r,
 	}
 
 	diag := http.Server{
