54536bb01bc43d9c1eaa7c3e3f424ad7f3a20bed
Elena Grahovac <elena@grahovac.me>
2019-09-28 11:20:31 +0200

    PORT=8080 DIAG_PORT=8181 go run ./cmd/paris/main.go

--- cmd/paris/main.go
+++ cmd/paris/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"net"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -12,14 +14,24 @@ func main() {
 
 	logger.Info("The aplication is starting...")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		logger.Fatal("Business logic port is not set")
+	}
+
+	diagPort := os.Getenv("DIAG_PORT")
+	if diagPort == "" {
+		logger.Fatal("Diagnostics port is not ste")
+	}
+
 	r := mux.NewRouter()
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    net.JoinHostPort("", port),
 		Handler: r,
 	}
 
 	diag := http.Server{
-		Addr: ":8081",
+		Addr: net.JoinHostPort("", diagPort),
 	}
 
 	go func() {
